ca30eb165411c46d772094fb77d5fcbb1c127926
vishalgddam873 <vishal18@navgurukul.org>
2023-01-24 15:32:38 +0530

    Inital Comment

---
+++ static/index.js
@@ -0,0 +1,60 @@
+var date = new Date()
+const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+display_date = `${weekDay[date.getDay()]}, ${date.getDate()} ${month[date.getMonth()]} ${date.getFullYear()}`
+
+let predicted_emotion;
+
+$(document).ready(function () {
+    $("#display_date").html(display_date)
+    $('#save_button').prop('disabled', true);
+})
+
+$(function () {
+    $("#predict_button").click(function () {
+        let input_data = {
+            "text": $("#text").val()
+        }
+        $.ajax({
+            type: 'POST',
+            url: "/predict-emotion",
+            data: JSON.stringify(input_data),
+            dataType: "json",
+            contentType: 'application/json',
+            success: function (result) {
+                $("#prediction").html(result.data.predicted_emotion)
+                $("#emo_img_url").attr('src', result.data.predicted_emotion_img_url);
+                $('#prediction').css("display", "");
+                $('#emo_img_url').css("display", "");
+                predicted_emotion = result.data.predicted_emotion
+                $('#save_button').prop('disabled', false);
+            },
+            error: function (result) {
+                alert(result.responseJSON.message)
+            }
+        });
+    });
+    $("#save_button").click(function () {
+        save_data = {
+            "date": display_date,
+            "text": $("#text").val(),
+            "emotion": predicted_emotion
+        }
+        $.ajax({
+            type: 'POST',
+            url: "/save-entry",
+            data: JSON.stringify(save_data),
+            dataType: "json",
+            contentType: 'application/json',
+            success: function () {
+                alert("Your entry has been saved successfully!")
+                window.location.reload()
+            },
+            error: function (result) {
+                alert(result.responseJSON.message)
+            }
+        });
+
+    });
+})
